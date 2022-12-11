57052ae4fd26a83434cb15140fdae5c001b78896
Window5 <13127531-Window5@users.noreply.gitlab.com>
2022-12-11 09:32:25 +0100

    Add library

---
+++ src/main/java/me/window/permissions/PermissionProvider.java
@@ -0,0 +1,48 @@
+package me.window.permissions;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.adventure.audience.Audiences;
+import net.minestom.server.entity.Player;
+import net.minestom.server.extensions.Extension;
+
+public class PermissionProvider {
+
+    String[] extensionPermissions;
+    int opLevel;
+
+    public PermissionProvider(String... extensionPermissions) {
+        this.extensionPermissions = extensionPermissions;
+        opLevel = 4;
+    }
+    public PermissionProvider(int opLevel, String... extensionPermission) {
+        this.extensionPermissions = extensionPermission;
+        this.opLevel = opLevel;
+    }
+
+    public boolean hasExtensionPermission(Player player, String permission) {
+        if (player.hasPermission("*")|| player.getPermissionLevel() >= opLevel)
+            return true;
+        for(String perm : extensionPermissions) {
+            if(player.hasPermission(perm + ".*")) return true;
+        }
+        return false;
+    }
+
+    public boolean hasExtensionPermission(Player player) {
+        if(player.hasPermission("*") || player.getPermissionLevel() >= opLevel) return true;
+        for(String perm : extensionPermissions) {
+            if(player.hasPermission(perm)) return true;
+        }
+        return false;
+    }
+
+    public static boolean hasPermission(Player player, String permission) {
+        return player.hasPermission("*") || player.hasPermission(permission) || player.getPermissionLevel() >= 4;
+    }
+
+    public static boolean hasPermission(Player player, String permission, int opLevel) {
+        return player.hasPermission("*") || player.hasPermission(permission) || player.getPermissionLevel() >= opLevel;
+    }
+
+}
